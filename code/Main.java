3a98eb765aca7af75a9d1133db9d82799186a3c0
Roger <rogermhetre18@gmail.com>
2026-07-13 23:05:35 +0530

    some math

--- code/Main.java
+++ code/Main.java
@@ -1,18 +1,29 @@
-import java.util.Random;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        Random random = new Random();
+        // circumeference = 2 * Math.PI * radius;
+        // area = Math.PI * radius * radius;
+        // volume = (4.0 / 3.0) * Math.PI * radius * radius * radius;
 
-        boolean isHeads;
+        Scanner scanner = new Scanner(System.in);
 
-        isHeads = random.nextBoolean();
+        double radius;
+        double circumference;
+        double area;
+        double volume;
 
-        if (isHeads) {
-            System.out.println("HEADS");
-        } else {
-            System.out.println("TAILS");
-        }
+        System.out.print("Enter the radius: ");
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0/ 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("The circumference is: %.1fcm\n", circumference);
+        System.out.printf("The area is: %.1fcm²\n", area );
+        System.out.printf("The volume is: %.1fcm³\n", volume );
+
+        scanner.close();
     }
 }
