003305a1d1842839d03cdfea252fe197d27b7492
Roger <rogermhetre18@gmail.com>
2026-07-21 07:38:09 +0530

    Coding

---
+++ code/CI_calculator.java
@@ -0,0 +1,32 @@
+// Compound Interest Calculator
+
+import java.util.Scanner;
+
+public class CI_calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double principal, rate, amount;
+        int timesCompounded, years;
+
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the rate of interest(%): ");
+        rate = scanner.nextDouble()/100;
+
+        System.out.print("Enter the number of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate/timesCompounded, timesCompounded * years);
+
+        System.out.printf("The amount after %d years is %.2f", years, amount );
+
+        scanner.close();
+
+    }
+}
--- code/Main.java
+++ code/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        // printf() = is a method used to format output
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
 
-        // %[flags][width][.precision][specifier-character]
+        System.out.printf("%d\n", id1);
+        System.out.printf("%d\n", id2);
+        System.out.printf("%d\n", id3);
+        System.out.printf("%d\n", id4);
 
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
-
-        System.out.printf("% .1f\n", price1);
-        System.out.printf("% .1f\n", price2);
-        System.out.printf("% .1f\n", price3);
 
     }
 }
