d08b89d151bf6e43e9fc675f6aaf9a3f7792a016
Roger <rogermhetre18@gmail.com>
2026-07-31 20:51:22 +0530

    Trying out methods

--- code/methodsBlock.java
+++ code/methodsBlock.java
@@ -1,18 +1,34 @@
 public class methodsBlock {
     public static void main(String[] args) {
-        String name = "Juice wrld";
-        int age = 26;
 
-        happyBirthday(name, age);
-        happyBirthday(name, age);
-        happyBirthday(name, age);
-    }
-    static void happyBirthday(String name, int age) {
-        System.out.println("Happy birthday juice wrld");
-        System.out.printf("Happy birthday dear %s\n", name);
-        System.out.printf("You are %d years old\n", age);
-        System.out.println("Happy birthday dear juice wrld");
-        System.out.println();
+        System.out.println(square(5));
+        System.out.println(cube(5));
+        String fullName = getFullName("Juice", "Wrld");
+        System.out.println(fullName);
+
+        int age = 17;
+
+        if (ageCheck(age)){
+            System.out.println("You may sign up ");
+        } else {
+            System.out.println("You are too young to sign up");
+        }
+
     }
 
+    static double square(double number){return number * number;}
+
+    static double cube(double number) {return number * number * number;}
+
+    static String getFullName(String firstName, String lastName){
+        return firstName + " " + lastName;
+    }
+
+    static boolean ageCheck(int age){
+        if (age >= 18){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
