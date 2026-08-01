dcbc4c9ec1cab0d371a7b2cd0aaf544813b42ccc
Roger <rogermhetre18@gmail.com>
2026-08-01 23:24:00 +0530

    Making something

---
+++ code/bankingProgram.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bankingProgram {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double balance;
+        boolean isRunning = true;
+        int choice;
+
+        System.out.println("===============================");
+        System.out.println("Welcome to the Banking Program!");
+        System.out.println("===============================");
+        System.out.println("1. Show Balance");
+        System.out.println
+
+        scanner.close();
+    }
+}
