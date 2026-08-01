e92edea77d2dd7546eb9c5f6e80e15d42c467e3d
Roger <rogermhetre18@gmail.com>
2026-08-01 22:29:19 +0530

    Practicing some code

--- code/overloadedMethod.java
+++ code/overloadedMethod.java
@@ -1,16 +1,18 @@
 public class overloadedMethod {
     public static void main(String[] args) {
 
-        System.out.println(add(1,2, 3));
+        String pizza = bakePizza("flat bread", "Mozzarella", "pepperoni");
+
+        System.out.println(pizza);
 
     }
-    static double add(double a, double b){
-        return a+b;
+    static String bakePizza(String bread){
+        return bread + " Pizza";
     }
-    static double add(double a, double b, double c){
-        return a+b+c;
+    static String bakePizza(String bread, String cheese){
+        return cheese + " " + bread + " Pizza";
     }
-    static double add(double a, double b, double c, double d){
-        return a+b+c+d;
+    static String bakePizza(String bread, String cheese, String topping){
+        return topping + " " + cheese + " " + bread + " Pizza";
     }
 }
