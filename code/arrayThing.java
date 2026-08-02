5ca6b618fb571bc8db7a35964b952c3dd3121b39
Roger <rogermhetre18@gmail.com>
2026-08-02 21:00:00 +0530

    Going through array

---
+++ code/arrayThing.java
@@ -0,0 +1,9 @@
+public class arrayThing {
+    public static void main(String[] args){
+
+        String[] fruits = {"apple", "banana", "orange", "grape"};
+
+        System.out.println(fruits[0]);
+
+    }
+}
