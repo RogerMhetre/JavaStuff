d0a518f9e52a8aca10f54dcb6aaa20184f1bdc3b
Roger <rogermhetre18@gmail.com>
2026-08-05 23:04:35 +0530

    Quiz game

---
+++ code/quizGame.java
@@ -0,0 +1,13 @@
+public class quizGame {
+    public static void main(String[] args){
+
+        String[] questions = {"What is the main function of a router?",
+                              "Which part of the computer is considered the brain?",
+                              "What year was Facebook launched?",
+                              "Who is known as the father of computer?",
+                              "What was teh firsrt computer language?"};
+
+        String[][] options = {
+
+    }
+}
