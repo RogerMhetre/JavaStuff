5712d0c8bcb3ec26faabd59ec64df4baae2f5b97
Roger <rogermhetre18@gmail.com>
2026-08-07 23:24:42 +0530

    Slot machine

--- code/slotMachine.java
+++ code/slotMachine.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class slotMachine{
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet = 0;
+        int payout = 0;
+        String [] row;
+
+        System.out.println("=====================");
+        System.out.println("Welcome to Java Slots");
+        // System.out.println("Symbols: ◎ ☘ ☢ ♕ ♠ ♥ ♦ ♣ ♤ ♧ ♢ ☼ ☻ ☹ ✪ ✫ ✯ ✰ ✦ ✧ ✨ ❤ ❣ ❦ ❧ ❨ ❩ ❪ ❫ ❬ ❭ ❮ ❯ ⭐ 💎 🍀 💰 👑 🎰 🏆 🪙 💵 7️⃣ 🌟");
+        System.out.println("Symbols: ✪ ◎ ☘ ☢ ♕ ♠ ♥");
+        System.out.println("=====================");
+
+        while(balance > 0){
+            System.out.println("Current balance: $"
 
     }
 }
