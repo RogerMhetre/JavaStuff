ca62807407aa000b1fede188f242a2e7348cd383
Roger <rogermhetre18@gmail.com>
2026-08-07 20:45:45 +0530

    Rock paper scissors game

--- code/rockPaperScissors.java
+++ code/rockPaperScissors.java
@@ -12,30 +12,41 @@ public class rockPaperScissors {
         String computerChoice;
         String playAgain = "yes";
 
-        System.out.println("-------------------------------");
-        System.out.println("Welcome to Rock Paper Scissors!");
-        System.out.println("-------------------------------");
-        System.out.println();
+        do{
+            System.out.println("-------------------------------");
+            System.out.println("Welcome to Rock Paper Scissors!");
+            System.out.println("-------------------------------");
+            System.out.println();
 
-        System.out.print("Enter your move (rock, paper, scissors): ");
-        playerChoice = scanner.nextLine().toLowerCase();
+            System.out.print("Enter your move (rock, paper, scissors): ");
+            playerChoice = scanner.nextLine().toLowerCase();
 
-        if(!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")){
-            System.out.println("Invalid choice");
-        }
+            if(!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")){
+                System.out.println("Invalid choice");
+                continue;
+            }
 
-        computerChoice = choices[random.nextInt(3)];
-        System.out.println("Computer chose: " + computerChoice);
+            computerChoice = choices[random.nextInt(3)];
+            System.out.println("Computer chose: " + computerChoice);
 
-        if(playerChoice.equals(computerChoice)){
-            System.out.println("It's a tie!");
-        }
-        else if(playerChoice.equals("rock") && computerChoice.equals("scissors") ||
-                playerChoice.equals("paper") && computerChoice.equals("rock") ||
-                playerChoice.equals("scissors") && computerChoice.equals("paper")){
-            System.out.println("You win!");
-        }
+            if(playerChoice.equals(computerChoice)){
+                System.out.println("It's a tie!");
+            }
+            else if(playerChoice.equals("rock") && computerChoice.equals("scissors") ||
+                    playerChoice.equals("paper") && computerChoice.equals("rock") ||
+                    playerChoice.equals("scissors") && computerChoice.equals("paper")){
+                System.out.println("You win!");
+                    }
+            else{
+                System.out.println("You lose!!!");
+            }
 
+            System.out.print("Play again? (yes/no): ");
+            playAgain = scanner.nextLine().toLowerCase();
+
+        }while(playAgain.equals("yes"));
+
+        System.out.println("Thanks for playing!!!");
 
         scanner.close();
     }
