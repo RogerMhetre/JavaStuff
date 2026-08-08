7953a7203b074f245094f1c72ec85ea951e199f0
Roger <rogermhetre18@gmail.com>
2026-08-08 14:10:00 +0530

    doing something with class

--- code/classThing.java
+++ code/classThing.java
@@ -1,12 +1,11 @@
 public class classThing{
     public static void main(String[] args){
 
-        Car car = new Car();
+        Car car1 = new Car();
+        Car car2 = new Car();
 
-        System.out.println(car.isRunning);
-        car.start();
-        System.out.println(car.isRunning);
-        car.stop();
-        System.out.println(car.isRunning);
+        System.out.println(car1.make + " " + car1.model);
+        System.out.println(car2.make + " " + car2.model);
+
     }
 }
