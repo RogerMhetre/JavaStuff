4d9a926eb1687184af4410fa017922a19c0fad56
Roger <rogermhetre18@gmail.com>
2026-08-08 20:53:32 +0530

    Commit

---
+++ code/Friend.java
@@ -0,0 +1,10 @@
+public class Friend {
+
+    int numOfFriends;
+    String name;
+
+    Friend(String name) {
+        this.name = name;
+        numOfFriends++;
+    }
+}
---
+++ code/staticThing.java
@@ -0,0 +1,11 @@
+public class staticThing {
+    public static void main(String[] args) {
+
+        Friend friend1 = new Friend("Roger");
+        Friend friend2 = new Friend("Drake");
+
+        System.out.println(friend1.numOfFriends);
+        System.out.println(friend2.numOfFriends);
+
+    }
+}
