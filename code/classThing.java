46607874ac97f04e83faf2b988ac6f6fdef89455
Roger <rogermhetre18@gmail.com>
2026-08-08 13:32:54 +0530

    Trying out classes

---
+++ code/classThing.java
@@ -0,0 +1,12 @@
+public class classThing{
+    public static void main(String[] args){
+
+        Car car = new Car();
+
+        System.out.println(car.isRunning);
+        car.start();
+        System.out.println(car.isRunning);
+        car.stop();
+        System.out.println(car.isRunning);
+    }
+}
