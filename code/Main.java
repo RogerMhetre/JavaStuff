75d7d08b5ef0166cc16fe164cba0b79cedb606ed
Roger <rogermhetre18@gmail.com>
2026-08-11 20:59:00 +0530

    Initial commit

--- code/Main.java
+++ code/Main.java
@@ -1,4 +1,12 @@
 public class Main {
     public static void main(String[] args){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(i == 0 && j == 0){
+                    continue;
+                }
+                System.out.println("i: " + i + " j: " + j);
+            }
+        }
     }
 }
