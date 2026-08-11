974111b0e4d86a95ba1404bd2ff4127cd70a932b
Roger <rogermhetre18@gmail.com>
2026-08-11 23:08:31 +0530

    Initial commit

--- code/Main.java
+++ code/Main.java
@@ -1,12 +1,31 @@
-public class Main {
-    public static void main(String[] args){
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(i == 0 && j == 0){
-                    continue;
-                }
-                System.out.println("i: " + i + " j: " + j);
-            }
+class Car {
+    String brand;
+    int price;
+
+    void display(){
+        System.out.println("Car brand: " + this.brand);
+        System.out.println("Car price: " + "$" + this.price);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        car1.brand = "Challenger";
+        car1.price = 30000;
+        car1.display();
+
+        car2.brand = "Corvette";
+        car2.price = 25000;
+        car2.display();
+
+        int a = 1;
+
+        switch(a){
+            case 1 -> System.out.println("a is 1");
+            case 2 -> System.out.println("a is 2");
         }
     }
 }
